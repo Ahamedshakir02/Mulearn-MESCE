0ff5d26e9faf57a440984ae44806aebd4880ed1d
ahamedshakir <ahamedshakir02@gmail.com>
2025-09-25 21:57:50 +0530

    added show more and show less and its style

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,11 +1,56 @@
+import React, { useState } from "react";
 import styles from "./About.module.css";
-import data from "../../../data.json"
+import data from "../../../data.json";
 
 const About = () => {
+    const paragraphs = data.about.split('\n\n');
+    const [showAll, setShowAll] = useState(false);
+
+    const handleToggle = (e: React.MouseEvent) => {
+        e.preventDefault();
+        setShowAll((prev) => !prev);
+    };
+
     return (
         <div id="about" className={styles.AboutWrapper}>
             <h1>About Us</h1>
-            <p>{data.about}</p>
+            {!showAll ? (
+                <p>
+                    {paragraphs[0]}
+                    {paragraphs.length > 1 && (
+                        <>
+                            ...{" "}
+                            <a
+                                href="#"
+                                className={styles.showMoreLink}
+                                onClick={handleToggle}
+                            >
+                                Show more
+                            </a>
+                        </>
+                    )}
+                </p>
+            ) : (
+                <>
+                    {paragraphs.map((para, idx) => (
+                        <p key={idx}>
+                            {para}
+                            {idx === paragraphs.length - 1 && (
+                                <>
+                                    {" "}
+                                    <a
+                                        href="#"
+                                        className={styles.showMoreLink}
+                                        onClick={handleToggle}
+                                    >
+                                        Show less
+                                    </a>
+                                </>
+                            )}
+                        </p>
+                    ))}
+                </>
+            )}
         </div>
     );
 };
